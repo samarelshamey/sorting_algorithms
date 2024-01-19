edf2688deb803017cdef1d02aea77d259cdbd162
root <smsmelshamey90@gmail.com>
2024-01-19 05:09:39 -0800

    sorting algorithms

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,10 +1,52 @@
 #include "sort.h"
 
 /**
+ * swap_func - swap two elements
+ * @x: first element
+ * @y: second element
+ * Return: nothing
+*/
+
+void swap_func(listint_t *x, listint_t *y)
+{
+	if (x->prev)
+		x->prev->next = y;
+	if (y->next)
+		y->next->prev = x;
+	x->next = y->next;
+	y->prev = x->prev;
+	x->prev = y;
+	y->next = x;
+}
+
+/**
  * insertion_sort_list - sorts doubly linked list of int in ascending order
  * @list: list to be sorted
  * Return: nothing
 */
+
 void insertion_sort_list(listint_t **list)
 {
+	listint_t *i, *j;
+
+	if (!list || !*list || !(*list)->next)
+		return;
+	i = (*list)->next;
+	while (i)
+	{
+		j = i;
+		i = i->next;
+		while (j && j->prev)
+		{
+			if (j->prev->n > j->n)
+			{
+				swap_func(j->prev, j);
+				if (!j->prev)
+					*list = j;
+				print_list((const listint_t *)*list);
+			}
+			else
+				j = j->prev;
+		}
+	}
 }
