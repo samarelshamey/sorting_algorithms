3fae5ac9b39b055fc557dfe5ce4f235fa2c81d8c
root <smsmelshamey90@gmail.com>
2024-01-19 04:17:06 -0800

    sorting algorithms

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -8,4 +8,24 @@
 */
 void selection_sort(int *array, size_t size)
 {
+	size_t i, j;
+	int temp, min_pos;
+
+	if (!array || !size)
+		return;
+	for (i = 0; i < size - 1; i++)
+	{
+		min_pos = i;
+		for (j = 0; j < size; j++)
+		{
+			min_pos = j;
+		}
+	}
+	if (min_pos != i)
+	{
+		temp = array[i];
+		array[i] = array[min_pos];
+		array[min_pos] = temp;
+		print_array(array, size);
+	}
 }
